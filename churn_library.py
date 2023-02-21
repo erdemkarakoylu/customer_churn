ed26f68b24c3e32c552720ba4ba7510d829bf098
erdemkarakoylu <erdemk@protonmail.com>
2023-02-20 20:54:15 -0500

    plotting functions for train_models

--- churn_library.py
+++ churn_library.py
@@ -8,12 +8,14 @@ os.environ['QT_QPA_PLATFORM']='offscreen'
 
 
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import plot_roc_curve, classification_report
 
 from constants import CAT_COLUMNS, KEEP_COLUMNS
 
@@ -123,12 +125,43 @@ def classification_report_image(y_train,
     output:
              None
     '''
-    pass
+    f, ax = plt.subplots(figsize=(5, 5))
+    ax.text(0.01, 1.25, str('Random Forest Train'), {'fontsize': 10}, fontproperties = 'monospace')
+    ax.text(0.01, 0.05, str(classification_report(y_test, y_test_preds_rf)), {'fontsize': 10}, fontproperties = 'monospace') # approach improved by OP -> monospace!
+    ax.text(0.01, 0.6, str('Random Forest Test'), {'fontsize': 10}, fontproperties = 'monospace')
+    ax.text(0.01, 0.7, str(classification_report(y_train, y_train_preds_rf)), {'fontsize': 10}, fontproperties = 'monospace') # approach improved by OP -> monospace!
+    ax.set_visible('off')
+    f.savefig('./images/results/cls_rep_rf.png',)
+
+    f, ax = plt.subplots(figsize=(5, 5))
+    ax.text(0.01, 1.25, str('Logistic Regression Train'), {'fontsize': 10}, fontproperties = 'monospace')
+    ax.text(0.01, 0.05, str(classification_report(y_train, y_train_preds_lr)), {'fontsize': 10}, fontproperties = 'monospace') # approach improved by OP -> monospace!
+    ax.text(0.01, 0.6, str('Logistic Regression Test'), {'fontsize': 10}, fontproperties = 'monospace')
+    ax.text(0.01, 0.7, str(classification_report(y_test, y_test_preds_lr)), {'fontsize': 10}, fontproperties = 'monospace') # approach improved by OP -> monospace!
+    ax.set_visible('off')
+    f.savefig('./images/results/cls_rep_lr.png')
 
 
-def feature_importance_plot(model, X_data, output_pth):
+def plot_roc_curve(X_data, y_data, model, out_pth):
     '''
-    creates and stores the feature importances in pth
+    creates and stores the feature importances in out_pth
+    input:
+            X_data: pandas dataframe of X values
+            y_data: pandas series of response value
+            model: pre-trained model
+            out_pth: path to store the figure
+
+    output:
+             None
+    '''
+    f, ax = plt.subplots(figsize=(15, 8))
+    plot_roc_curve(model, X_data, y_data, ax=ax)
+    f.savefig(out_pth, dpi=300)
+
+
+def feature_importance_plot(rf_model, X_data, output_pth):
+    '''
+    creates and stores the feature importances in output_pth
     input:
             model: model object containing feature_importances_
             X_data: pandas dataframe of X values
@@ -137,7 +170,13 @@ def feature_importance_plot(model, X_data, output_pth):
     output:
              None
     '''
-    pass
+    # Calculate feature importances
+    importances=rf_model.feature_importances_
+    # Sort feature importances in descending order
+    indices = np.argsort(importances)[::-1]
+
+# Rearrange feature names so they match the sorted feature importances
+names = [X.columns[i] for i in indices]
 
 def train_models(X_train, X_test, y_train, y_test, test_mode=False):
     '''
@@ -167,6 +206,9 @@ def train_models(X_train, X_test, y_train, y_test, test_mode=False):
         }
         cv_rfc = GridSearchCV(estimator=rfc, param_grid=param_grid, cv=5)
         cv_rfc.fit(X_train, y_train)
+        # save best model
+        joblib.dump(cv_rfc.best_estimator_, './models/rfc_model.pkl')
+        joblib.dump(lrc, './models/logistic_model.pkl')
         rfc_model = cv_rfc.best_estimator_
 
         lrc.fit(X_train, y_train)
@@ -177,3 +219,13 @@ def train_models(X_train, X_test, y_train, y_test, test_mode=False):
 
     y_train_preds_lr = lr_model.predict(X_train)
     y_test_preds_lr = lr_model.predict(X_test)
+
+    plot_roc_curve(
+        X_test, y_test, model=lr_model, 
+        save_path="./images/results/lr_roc_curve.png")
+    plot_roc_curve(
+        X_test, y_test, model=rfc_model, 
+        save_path="./images/results/rf_roc_curve.png"
+    )
+
+    feature_importance_plot()

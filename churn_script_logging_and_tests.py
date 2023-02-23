6b7d6c3a2184a395bce317ec997e519976d5b2d5
erdemkarakoylu <erdemk@protonmail.com>
2023-02-23 14:54:20 -0500

    added test module docstring

--- churn_script_logging_and_tests.py
+++ churn_script_logging_and_tests.py
@@ -1,7 +1,9 @@
+"""This module contains tests for all functions in churn_library.py.
+Tests were written for pytest and can be invoked from the command line either with
+pytest or (i)python."""
 from pathlib import Path
 import logging
 
-import numpy as np
 import pytest
 
 import churn_library as cl
@@ -16,103 +18,120 @@ logging.basicConfig(
 
 @pytest.fixture(scope='session')
 def lr_roc_curve_path():
+    """Pytest fixture for Logistic Regression ROC plot path."""
     return './images/results/lr_roc_curve.png'
 
 
 @pytest.fixture(scope='session')
 def rf_roc_curve_path():
+    """Pytest fixture for Random Forest ROC plot path."""
     return './images/results/rf_roc_curve.png'
 
 
 @pytest.fixture(scope='session')
 def lr_cls_rep_path():
+    "Pytest fixture for LR classification report image path."
     return './images/results/lr_cls_rep.png'
 
 
 @pytest.fixture(scope='session')
 def rf_cls_rep_path():
+    "Pytest fixture for RF classification report image path."
     return './images/results/rf_cls_rep.png'
 
 
 @pytest.fixture(scope='session')
 def rf_feat_imp_path():
+    "Pytest fixture for RF feature importance plot path."
     return './images/results/rf_feat_imp.png'
 
 
 @pytest.fixture(scope='session')
 def logistic_model_path():
+    "Pytest fixture for LR model file path."
     return './models/logistic_model.pkl'
 
 
 @pytest.fixture(scope='session')
 def random_forest_model_path():
+    "Pytest fixture for RF model file path."
     return './models/rfc_model.pkl'
 
 
 @pytest.fixture(scope='session')
 def keep_cols():
+    "Pytest fixture for input column to keep for modeling."
     return KEEP_COLUMNS
 
 
 @pytest.fixture(scope='session')
 def cat_cols():
+    "Pytest fixture for categorical columns in input data."
     return CAT_COLUMNS
 
 
 @pytest.fixture(scope="session")
 def image_dir():
+    "Pytest fixture for path to images directory."
     return "./images"
 
 
 @pytest.fixture(scope="session")
 def df_path():
+    "Pytest fixture for path to data used for modeling."
     return "./data/bank_data.csv"
 
 
 @pytest.fixture(scope="session")
 def dataframe(df_path):
+    "Pytest fixture for dataframe containing data."
     return cl.import_data(df_path)
 
 
 @pytest.fixture
 def train_models():
+    "Pytest fixture for train_models function in churn_library.py."
     return cl.train_models
 
 
 @pytest.fixture
 def perform_feature_engineering():
+    "Pytest fixture for perform_feature_engineering func. in churn_library.py."
     return cl.perform_feature_engineering
 
 
 @pytest.fixture
 def encoder_helper():
+    "Pytest fixture for encoder_helper function in churn_library.py."
     return cl.encoder_helper
 
 
 @pytest.fixture
 def perform_eda():
+    "Pytest fixture for perform_eda function in churn_library.py"
     return cl.perform_eda
 
 
 @pytest.fixture
 def import_data():
+    "Pytest fixture for import_data function in churn_library.py"
     return cl.import_data
 
 
 def test_import(import_data, df_path):
     '''
-    test data import - this example is completed for you to assist with the other test functions
+    Test of import_data function in churn_library.py.
     '''
     try:
-        df = import_data(df_path)
+        dframe = import_data(df_path)
         logging.info("Testing import_data: SUCCESS")
     except FileNotFoundError as err:
         logging.error("Testing import_eda: The file wasn't found")
         raise err
 
     try:
-        assert df.shape[0] > 0
-        assert df.shape[1] > 0
+        assert dframe.shape[0] > 0
+        assert dframe.shape[1] > 0
     except AssertionError as err:
         logging.error(
             "Testing import_data: The file doesn't appear to have rows and columns")
@@ -131,7 +150,7 @@ def test_import(import_data, df_path):
 )
 def test_perform_eda(perform_eda, dataframe, image_dir, eda_img_path):
     '''
-    test perform eda function.
+    Test of perform_eda function.
     '''
     logging.info("Testing perform_eda...")
     perform_eda(dataframe)
@@ -146,16 +165,16 @@ def test_perform_eda(perform_eda, dataframe, image_dir, eda_img_path):
 
 def test_encoder_helper(encoder_helper, dataframe, cat_cols):
     '''
-    test encoder helper
+    Test of encoder_helper function in churn_library.py.
     '''
     response = 'Churn'
     encoded_cols = [col + '_' + response for col in cat_cols]
-    df = encoder_helper(dataframe, cat_cols, response=response)
+    dframe = encoder_helper(dataframe, cat_cols, response=response)
     encoded_cols_set = set(encoded_cols)
     try:
-        assert set(df.columns.to_list()).intersection(
+        assert set(dframe.columns.to_list()).intersection(
             encoded_cols_set) == encoded_cols_set
-        assert df[encoded_cols].shape[0] > 0
+        assert dframe[encoded_cols].shape[0] > 0
         logging.info("Testing encoder_helper: SUCCESS!")
     except AssertionError as err:
         logging.error(
@@ -165,15 +184,15 @@ def test_encoder_helper(encoder_helper, dataframe, cat_cols):
 
 def test_perform_feature_engineering(perform_feature_engineering, dataframe):
     '''
-    test perform_feature_engineering
+    Test of perform_feature_engineering function in churn_library.py.
     '''
     logging.info("Testing perform_feature_engineering...")
-    X_train, X_test, y_train, y_test = perform_feature_engineering(
+    x_train, x_test, y_train, y_test = perform_feature_engineering(
         dataframe, response='Churn')
 
     try:
-        assert X_train.size > 0
-        assert X_test.size > 0
+        assert x_train.size > 0
+        assert x_test.size > 0
         assert y_train.size > 0
         assert y_test.size > 0
         logging.info("Success")
@@ -198,13 +217,13 @@ def test_train_models(
     image_dir, result_plot_path
 ):
     '''
-    test train_models.
+    Test of train_models function in churn_library.py.
     Note: adding "test mode" to train_models to avoid training models during testing.
     '''
     logging.info('Testing train_models...')
-    X_train, X_test, y_train, y_test = perform_feature_engineering(
+    x_train, x_test, y_train, y_test = perform_feature_engineering(
         dataframe, response='Churn')
-    train_models(X_train, X_test, y_train, y_test, test_mode=True)
+    train_models(x_train, x_test, y_train, y_test, test_mode=True)
     try:
         assert Path(logistic_model_path).exists()
         logging.info("Saved LR model file found.")
